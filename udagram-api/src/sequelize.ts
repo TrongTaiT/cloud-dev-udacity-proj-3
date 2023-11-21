03f07096e6e70c2190c0e60e91b54043497b8172
taint47 <taint47.fsoft@gmail.com>
2023-11-22 00:11:35 +0700

    fix sequelize.ts

--- udagram-api/src/sequelize.ts
+++ udagram-api/src/sequelize.ts
@@ -1,13 +1,25 @@
-import {Sequelize} from 'sequelize-typescript';
-import {config} from './config/config';
-
+import { Sequelize } from "sequelize-typescript";
+import { config } from "./config/config";
 
 export const sequelize = new Sequelize({
-  'username': config.username,
-  'password': config.password,
-  'database': config.database,
-  'host': config.host,
-
-  'dialect': config.dialect,
-  'storage': ':memory:',
+   username: config.username,
+   password: config.password,
+   database: config.database,
+   host: config.host,
+   port: 5432,
+   dialect: config.dialect,
+   storage: ":memory:",
+   ssl: true,
+   dialectOptions: {
+      ssl: {
+         require: true,
+         rejectUnauthorized: false,
+      },
+   },
+   pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
 });
